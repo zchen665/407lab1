58e0928fd2c993195e7682c199d108f4ce290afa
zchen665 <azchen665@gmail.com>
2021-09-16 15:20:17 -0500

    initial edit

--- app/src/main/java/com/example/a407lab1/MainActivity.java
+++ app/src/main/java/com/example/a407lab1/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.a407lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imgView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
